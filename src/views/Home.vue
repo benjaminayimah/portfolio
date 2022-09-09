e3a900b345315a98e044ef7af9b6d3e1a3e4e49f
Benjamin <benjaminayimah@gmail.com>
2022-09-09 22:16:20 +0000

    d

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div id="home" class="main-row">
     <div class="section-wrapper">
       <intro />
+      <h3 class="mt-0">Here are some of my work</h3>
     </div>
   </div>
 </template>
@@ -14,3 +15,16 @@ export default {
   name: 'Home',
 }
 </script>
+<style lang="scss" scoped>
+h3{
+  position: relative;
+  &::before {
+    position: absolute;
+    content: "";
+    border-bottom: 2px solid;
+    border-color: #3FD68A;
+    width: 46px;
+    bottom: -5px;
+  }
+}
+</style>
