e60ae67d457d51da5beab6d9ab7e9778466648d3
Benjamin <benjaminayimah@gmail.com>
2022-09-18 23:13:53 +0000

    commit

--- src/components/includes/ProjectList.vue
+++ src/components/includes/ProjectList.vue
@@ -2,7 +2,7 @@
     <div class="project-hold relative">
         <div class="project-main-wrapper bg-secondary br-16" @click.prevent="$store.commit('showProject', project.id)">
             <img :src="project.image" loading="lazy" alt="">
-            <div class="caption">
+            <div class="caption fx-col gap-8">
                 <div class="title">Project name</div>
                 <div class="description">I’m a UI/UX Designer who focuses on crafting a delightful digital experience for users. </div>
             </div>
