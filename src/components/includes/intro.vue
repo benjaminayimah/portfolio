1c0decc2921917fda092f400c86556e1a0e09165
Benjamin <benjaminayimah@gmail.com>
2022-09-20 23:43:22 +0000

    commit project details

--- src/components/includes/intro.vue
+++ src/components/includes/intro.vue
@@ -2,7 +2,7 @@
     <section>
         <div class="fx gap-16 h1 fw-500">Hi <span>👋🏽</span></div>
         <div class="h1 fw-600">I'm Ben</div>
-        <p>I’m a UI/UX Designer who focuses on crafting a delightful digital experience for users. Needless to say, I advocate for users 👩🏾‍💻👨🏽‍💻👨‍💻
+        <p>I’m a UI/UX Designer who focuses on crafting a delightful digital experience for users. <br/> Needless to say, I advocate for the user 👩🏾‍💻👨🏽‍💻👨‍💻
         </p>
     </section>
 </template>
