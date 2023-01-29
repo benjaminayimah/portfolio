71907dd712eb9a41f940a10d342c9ddbb34f68cd
Benjamin Ayimah <benjaminayimah@gmail.com>
2023-01-29 14:12:16 +0000

    Add files via upload

---
+++ precache-manifest.16d820b8131c63ca58f16c5155d46b39.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5fa0c77133db0fea5c",
+    "url": "/css/app.b50eb9e4.css"
+  },
+  {
+    "revision": "b6bb93d5c8a60a62c827f31d91b6cea9",
+    "url": "/favemeicom.png"
+  },
+  {
+    "revision": "320aeaf6f21cdf3752d3bf257405c01b",
+    "url": "/img/behance_shots.320aeaf6.jpg"
+  },
+  {
+    "revision": "8945ddeb6136c752bac0390117d0731f",
+    "url": "/img/ben.8945ddeb.png"
+  },
+  {
+    "revision": "40959d043d2b9a10ca1cc132f7c5b7ce",
+    "url": "/img/competitive_audit.40959d04.png"
+  },
+  {
+    "revision": "b6bb93d5c8a60a62c827f31d91b6cea9",
+    "url": "/img/favemeicom.b6bb93d5.png"
+  },
+  {
+    "revision": "63a2040b6cf8d75f47e0c820082fdea7",
+    "url": "/img/flexsale-banner.63a2040b.svg"
+  },
+  {
+    "revision": "cc97b82bcc8df464acbe7241d92f31b3",
+    "url": "/img/flexsale-banner.cc97b82b.png"
+  },
+  {
+    "revision": "b55124781e7dea0bb368e6b14ba663e1",
+    "url": "/img/flexsale-refined-1.b5512478.png"
+  },
+  {
+    "revision": "1aa05f5d589a6ced136429d8a48b26ee",
+    "url": "/img/flexsale-refined-2.1aa05f5d.png"
+  },
+  {
+    "revision": "5fa16e452a0bed5100c7bbcb3c6c9460",
+    "url": "/img/flexsale-refined-3.5fa16e45.png"
+  },
+  {
+    "revision": "4a21ed721fcb11d1bb7e26a842fc9cd8",
+    "url": "/img/flexsale-refined-4.4a21ed72.png"
+  },
+  {
+    "revision": "1aa05f5d589a6ced136429d8a48b26ee",
+    "url": "/img/flexsale-refined-5.1aa05f5d.png"
+  },
+  {
+    "revision": "f4962fe65cb59abcac8d97e1903f2c27",
+    "url": "/img/flexsale-refined-6.f4962fe6.png"
+  },
+  {
+    "revision": "d01ba3ae6e1a24ebbe95053aae19ef32",
+    "url": "/img/flexsale-refined-7.d01ba3ae.png"
+  },
+  {
+    "revision": "065659b3b3ac16a97eae4523fe319f1d",
+    "url": "/img/flexsale-refined-8.065659b3.png"
+  },
+  {
+    "revision": "ee66c73eacabf4132f25b046edcf9b4e",
+    "url": "/img/flexsale-refined-9.ee66c73e.png"
+  },
+  {
+    "revision": "553f26d281834c0b7d62b4fa9425443b",
+    "url": "/img/flexsale.553f26d2.png"
+  },
+  {
+    "revision": "d35595951791941e5f992f04d075337a",
+    "url": "/img/flexsale_featured.d3559595.png"
+  },
+  {
+    "revision": "84f715840edf1661cfa1521b35f40571",
+    "url": "/img/ideation.84f71584.png"
+  },
+  {
+    "revision": "b5cfb05d580572a05a5467be2c34535d",
+    "url": "/img/instagram-shots.b5cfb05d.jpg"
+  },
+  {
+    "revision": "9f9a9e36e4e795d63b7e48ffd9422a4c",
+    "url": "/img/lo-fi.9f9a9e36.png"
+  },
+  {
+    "revision": "d7553620a763c9e12524259ad7c88d32",
+    "url": "/img/paper_wireframes.d7553620.png"
+  },
+  {
+    "revision": "4b2330bb1d2ffd11c8332bd78f52e685",
+    "url": "/img/person-1.4b2330bb.png"
+  },
+  {
+    "revision": "7211ac60a87f07f79f2720a549baad77",
+    "url": "/img/prod_delight.7211ac60.svg"
+  },
+  {
+    "revision": "8618a0606a1de53fa2a12c09a1a1f16b",
+    "url": "/img/screen_variations.8618a060.png"
+  },
+  {
+    "revision": "98cbe231bcf6fd08b05437ac05b0d2fe",
+    "url": "/img/sitemap.98cbe231.png"
+  },
+  {
+    "revision": "715dc808df8d3174dc1ff77d95db90e9",
+    "url": "/img/ui.715dc808.svg"
+  },
+  {
+    "revision": "4628fc8826eaf0739663cdce2ed1d0e1",
+    "url": "/img/user_journey_map.4628fc88.png"
+  },
+  {
+    "revision": "21465150db3e85d33ca6cf36cdabc728",
+    "url": "/img/ux.21465150.svg"
+  },
+  {
+    "revision": "cb0374373647bf086aebb9ba1a8cc7a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d5fa0c77133db0fea5c",
+    "url": "/js/app.d2e79b64.js"
+  },
+  {
+    "revision": "4aa7bcfcfce5e63fa92e",
+    "url": "/js/chunk-vendors.7d383bc3.js"
+  },
+  {
+    "revision": "0328cb5657a846b1043cd038e6311e82",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
